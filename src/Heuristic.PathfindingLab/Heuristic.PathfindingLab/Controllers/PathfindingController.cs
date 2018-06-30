3e6aa8b3fa2525565bec5ac6366b809ec8472bb7
Robert Vandenberg Huang <rvh.omni@gmail.com>
2018-06-30 23:44:31 +0800

    Simplify source code

--- src/Heuristic.PathfindingLab/Heuristic.PathfindingLab/Controllers/PathfindingController.cs
+++ src/Heuristic.PathfindingLab/Heuristic.PathfindingLab/Controllers/PathfindingController.cs
@@ -15,15 +15,56 @@ namespace Heuristic.PathfindingLab.Controllers
         [HttpPost]
         public IEnumerable<Point> Find([FromBody]PathfindingRequestBody body)
         {
-            var unit = 1;
             var start = new Point(body.FromX, body.FromY);
             var goal = new Point(body.GoalX, body.GoalY);
             var boundary = new Rectangle(0, 0, body.Map.Length, body.Map.Max(row => row.Length));
+            var unit = 1;
             var obstacles = PathfindingRequestBody.GetAllObstacles(body.Map);
-            var queryable = PathfindingRequestBody.InitializeSearch(body.Algorithm, start, goal, unit).Except(obstacles).Where(boundary.Contains);
-            var solution = PathfindingRequestBody.ApplyHeuristicFunction(queryable, body.Heuristics);
+            var queryable = HeuristicSearch.Use(body.Algorithm, start, goal, (step, i) => step.GetFourDirections(unit));
+            var solution = from step in queryable.Except(obstacles)
+                           where boundary.Contains(step)
+                           select step;
 
-            return solution;
+            return ApplyHeuristicFunction(solution, body.Heuristics);
+        }
+
+        private static HeuristicSearchOrderBy<Point, Point> ApplyHeuristicFunction(HeuristicSearchBase<Point, Point> queryable, string[] heuristicNames)
+        {
+            var orderBy = default(HeuristicSearchOrderBy<Point, Point>);
+            var goal = queryable.To;
+
+            switch (heuristicNames.FirstOrDefault())
+            {
+                case nameof(PointExtensions.GetChebyshevDistance):
+                    orderBy = queryable.OrderBy(p => p.GetChebyshevDistance(goal));
+                    break;
+
+                case nameof(PointExtensions.GetEuclideanDistance):
+                    orderBy = queryable.OrderBy(p => p.GetEuclideanDistance(goal));
+                    break;
+
+                default:
+                    orderBy = queryable.OrderBy(p => p.GetManhattanDistance(goal));
+                    break;
+            }
+            foreach (var heuristicName in heuristicNames.Skip(1).Take(2))
+            {
+                switch (heuristicName)
+                {
+                    case nameof(PointExtensions.GetChebyshevDistance):
+                        orderBy = orderBy.ThenBy(p => p.GetChebyshevDistance(goal));
+                        break;
+
+                    case nameof(PointExtensions.GetEuclideanDistance):
+                        orderBy = orderBy.ThenBy(p => p.GetEuclideanDistance(goal));
+                        break;
+
+                    default:
+                        orderBy = orderBy.ThenBy(p => p.GetManhattanDistance(goal));
+                        break;
+                }
+            }
+            return orderBy;
         }
     }
 }
--- src/Heuristic.PathfindingLab/Heuristic.PathfindingLab/Models/PathfindingRequestBody.cs
+++ src/Heuristic.PathfindingLab/Heuristic.PathfindingLab/Models/PathfindingRequestBody.cs
@@ -1,24 +1,30 @@
-﻿using LinqToAStar;
+﻿using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
 
 namespace Heuristic.PathfindingLab.Models
 {
     public class PathfindingRequestBody
     {
+        [JsonProperty(Required = Required.Always)]
         public int FromX { get; set; }
 
+        [JsonProperty(Required = Required.Always)]
         public int FromY { get; set; }
 
+        [JsonProperty(Required = Required.Always)]
         public int GoalX { get; set; }
 
+        [JsonProperty(Required = Required.Always)]
         public int GoalY { get; set; }
 
+        [JsonProperty(Required = Required.Always)]
         public int?[][] Map { get; set; }
 
+        [JsonProperty(Required = Required.Always)]
         public string[] Heuristics { get; set; }
 
+        [JsonProperty(Required = Required.Always)]
         public string Algorithm { get; set; }
 
         public static IEnumerable<Point> GetAllObstacles(int?[][] map)
@@ -27,64 +33,6 @@ namespace Heuristic.PathfindingLab.Models
                 for (var x = 0; x < map[y].Length; x++)
                     if (map[y][x] == null)
                         yield return new Point(x, y);
-        }
-
-        public static HeuristicSearchBase<Point, Point> InitializeSearch(string algorithmName, Point start, Point goal, int unit)
-        {
-            switch (algorithmName)
-            {
-                case nameof(HeuristicSearch.AStar):
-                    return HeuristicSearch.AStar(start, goal, (s, i) => s.GetFourDirections(unit));
-
-                case nameof(HeuristicSearch.BestFirstSearch):
-                    return HeuristicSearch.BestFirstSearch(start, goal, (s, i) => s.GetFourDirections(unit));
-
-                case nameof(HeuristicSearch.IterativeDeepeningAStar):
-                    return HeuristicSearch.IterativeDeepeningAStar(start, goal, (s, i) => s.GetFourDirections(unit));
-
-                case nameof(HeuristicSearch.RecursiveBestFirstSearch):
-                    return HeuristicSearch.RecursiveBestFirstSearch(start, goal, (s, i) => s.GetFourDirections(unit));
-            }
-            return HeuristicSearch.AStar(start, goal, (s, i) => s.GetFourDirections(unit));
-        }
-
-        public static HeuristicSearchOrderBy<Point, Point> ApplyHeuristicFunction(HeuristicSearchBase<Point, Point> queryable, string[] heuristicNames)
-        {
-            var orderBy = default(HeuristicSearchOrderBy<Point, Point>);
-            var goal = queryable.To;
-
-            switch (heuristicNames.FirstOrDefault())
-            {
-                case nameof(PointExtensions.GetChebyshevDistance):
-                    orderBy = queryable.OrderBy(p => p.GetChebyshevDistance(goal));
-                    break;
-
-                case nameof(PointExtensions.GetEuclideanDistance):
-                    orderBy = queryable.OrderBy(p => p.GetEuclideanDistance(goal));
-                    break;
-
-                default:
-                    orderBy = queryable.OrderBy(p => p.GetManhattanDistance(goal));
-                    break;
-            }
-            foreach (var heuristicName in heuristicNames.Skip(1).Take(2))
-            {
-                switch (heuristicName)
-                {
-                    case nameof(PointExtensions.GetChebyshevDistance):
-                        orderBy = orderBy.ThenBy(p => p.GetChebyshevDistance(goal));
-                        break;
-
-                    case nameof(PointExtensions.GetEuclideanDistance):
-                        orderBy = orderBy.ThenBy(p => p.GetEuclideanDistance(goal));
-                        break;
-
-                    default:
-                        orderBy = orderBy.ThenBy(p => p.GetManhattanDistance(goal));
-                        break;
-                }
-            }
-            return orderBy;
         }
     }
 }
